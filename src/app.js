7d3ee590665ab03962ea58a927c9f3cf1126fc89
Farhan Mahmood <farhanm612@gmail.com>
2018-09-14 19:35:20 +0500

    added router

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route} from 'react-router-dom';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss'
+
+const ExpenseDashboardPage = () => {
+    <div>
+        This is from my dashboard component
+    </div>
+};
+
+const routes = (
+    <BrowserRouter>
+        <div>
+        <Route path="/" component={ExpenseDashboardPage}/>
+        </div>
+    </BrowserRouter>
+);
+
+ReactDOM.render(routes,document.getElementById('app'));
