3362a4ba4f8fe1ef836361397c832f200a71152e
Farhan Mahmood <farhanm612@gmail.com>
2018-09-19 19:17:00 +0500

    Organizing Routes

--- src/app.js
+++ src/app.js
@@ -1,21 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route} from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss'
+import AppRouter from './routers/AppRouter'
 
-const ExpenseDashboardPage = () => {
-    <div>
-        This is from my dashboard component
-    </div>
-};
 
-const routes = (
-    <BrowserRouter>
-        <div>
-        <Route path="/" component={ExpenseDashboardPage}/>
-        </div>
-    </BrowserRouter>
-);
-
-ReactDOM.render(routes,document.getElementById('app'));
+ReactDOM.render(<AppRouter/>,document.getElementById('app'));
