fe35eb0bb516056ae1187328fe35c2a672294919
Farhan Mahmood <farhanm612@gmail.com>
2018-09-24 22:58:36 +0500

    query url

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
-const EditExpensePage = () => (
-    <div>
-        This is from my edit expense component
-    </div>
-);
+const EditExpensePage = (props) => {
+    return (
+        <div>
+            Editing the expense of {props.match.params.id}
+        </div>
+    );
+};
 
 export default EditExpensePage;
